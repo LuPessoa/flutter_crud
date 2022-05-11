4e51a8e6f2098a001b4b170ed0c6255adf3e6828
Lucilene Pessoa <72807076+LuPessoa@users.noreply.github.com>
2022-05-11 16:18:13 -0300

    subindo projeto flutter

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+'1': const User(
+  id: '1',
+  name: 'Maria',
+  email:'maria@alunos.com.br',
+  avatarUrl: 'https://cdn.pixabay.com/photo/2021/10/31/16/53/squid-game-6758082_960_720.png'
+),
+
+'2': const User(
+  id: '2',
+  name: 'Rafaela',
+  email:'rafael@alunos.com.br',
+  avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807__340.png'
+),
+'3': const User(
+  id: '3',
+  name: 'Luana',
+  email:'luana@alunos.com.br',
+  avatarUrl: 'https://cdn.pixabay.com/photo/2021/01/17/09/11/woman-5924366_960_720.jpg'
+),
+'4': const User(
+  id: '4',
+  name: 'Junior',
+  email:'junior@alunos.com.br',
+  avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
+),
+'5': const User(
+  id: '4',
+  name: 'João',
+  email:'João@alunos.com.br',
+  avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png'
+),
+};
